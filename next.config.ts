3b8fe513b4ce6fb0c1f963aa64d67811be0f37e5
Wangzehao2009 <15866374+wangzehao2009@user.noreply.gitee.com>
2025-08-23 23:15:16 +0800

    add: add markdown custom

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 import createMDX from '@next/mdx';
 
 const withMDX = createMDX({
-  extension: /\.(md|mdx)$/,
+    extension: /\.(md|mdx)$/
 });
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['guwen-1252396323.cos.ap-chengdu.myqcloud.com', 'gxwtf.cn', 'account.gxwtf.cn'],
-  },
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx', 'js', 'jsx'],
+    images: {
+        domains: ['guwen-1252396323.cos.ap-chengdu.myqcloud.com', 'gxwtf.cn', 'account.gxwtf.cn'],
+    },
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx', 'js', 'jsx'],
 };
 
 export default withMDX(nextConfig);
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,32 @@
 import type { MDXComponents } from 'mdx/types'
-
-const components: MDXComponents = {}
-
+import Image, { ImageProps } from 'next/image'
+ 
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including inline styles,
+// components from other libraries, and more.
+ 
+const components = {
+  // Allows customizing built-in components, e.g. to add styling.
+  h2: ({ children }) => (
+        <h2 className="text-xl font-bold text-[var(--theme-color)]">
+      {children}
+    </h2>
+  ),
+  p: ({ children }) => (
+    <p className="text-primary">
+      {children}
+    </p>
+  ),
+  img: (props) => (
+    <Image
+      sizes="100vw"
+      style={{ width: '100%', height: 'auto' }}
+      {...(props as ImageProps)}
+    />
+  ),
+} satisfies MDXComponents
+ 
 export function useMDXComponents(): MDXComponents {
-    return components
+  return components
 }
