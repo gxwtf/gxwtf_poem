c93cd83d9ff37335715f403b55f15b0e19cbce1a
kelly <kellynew@outlook.com>
2025-08-24 12:31:54 +0800

    feat: iron-session

---
+++ src/app/(web)/test-session/page.tsx
@@ -0,0 +1,82 @@
+import Link from "next/link";
+import * as css from "./css";
+
+import { Metadata } from "next";
+import { Form } from "./form";
+
+export const metadata: Metadata = {
+    title: "🛠 iron-session examples: Client components, route handlers and SWR",
+};
+
+export default function AppRouterSWR() {
+    return (
+        <main className="p-10 space-y-5">
+            <p className="italic max-w-xl">
+                <u>How to test</u>: Login and refresh the page to see iron-session in
+                action. Bonus: open multiple tabs to see the state being reflected by
+                SWR automatically.
+            </p>
+
+            <div className="grid grid-cols-1 gap-4 p-10 border border-slate-500 rounded-md max-w-xl">
+                <Form />
+                <div className="space-y-2">
+                    <hr />
+                    <p>
+                        The following pages are protected and will redirect back here if
+                        you&apos;re not logged in:
+                    </p>
+                    {/* convert the following paragraphs into a ul li */}
+                    <ul className="list-disc list-inside">
+                        <li>
+                            <Link
+                                href="/test-session/protected"
+                                className={css.link}
+                            >
+                                Protected page via client component →
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <HowItWorks />
+        </main>
+    );
+}
+
+function HowItWorks() {
+    return (
+        <details className="max-w-2xl space-y-4">
+            <summary className="cursor-pointer">How it works</summary>
+
+            <ol className="list-decimal list-inside">
+                <li>
+                    During login, the form is submitted with SWR&apos;s{" "}
+                    <a
+                        href="https://swr.vercel.app/docs/mutation#useswrmutation"
+                        className={css.link}
+                    >
+                        useSWRMutation
+                    </a>
+                    . This makes a POST /session request using fetch.
+                </li>
+                <li>
+                    {" "}
+                    During logout, the form is submitted with SWR&apos;s{" "}
+                    <a
+                        href="https://swr.vercel.app/docs/mutation#useswrmutation"
+                        className={css.link}
+                    >
+                        useSWRMutation
+                    </a>
+                    . This makes a DELETE /session request using fetch.
+                </li>
+                <li>
+                    In all other places, the content of the session is optimistally
+                    rendered using the most recent value, and never gets outdated. This is
+                    automatically handled by SWR using mutations and revalidation.
+                </li>
+            </ol>
+        </details>
+    );
+}
---
+++ src/app/api/session/route.ts
@@ -0,0 +1,58 @@
+// Iron Session Server Routes /api/session
+
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import { defaultSession, sessionOptions } from "../../iron";
+import { sleep, SessionData } from "../../iron";
+
+// login
+export async function POST(request: NextRequest) {
+    const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+
+    const { username = "No username" } = (await request.json()) as {
+        username: string;
+    };
+
+    session.isLoggedIn = true;
+    session.username = username;
+    session.counter = 0;
+    await session.save();
+
+    // simulate looking up the user in db
+    await sleep(250);
+
+    return Response.json(session);
+}
+
+export async function PATCH() {
+    const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+
+    session.counter++;
+    await session.save();
+
+    return Response.json(session);
+}
+
+// read session
+export async function GET() {
+    const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+
+    // simulate looking up the user in db
+    await sleep(250);
+
+    if (session.isLoggedIn !== true) {
+        return Response.json(defaultSession);
+    }
+
+    return Response.json(session);
+}
+
+// logout
+export async function DELETE() {
+    const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+
+    session.destroy();
+
+    return Response.json(defaultSession);
+}
