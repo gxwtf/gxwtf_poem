12c7e25172085c05f6ef166fab8578f8f5aac6f9
kelly <kellynew@outlook.com>
2025-08-24 20:17:27 +0800

    update: update links

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -184,9 +184,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
+          <NavMain items={data.navMain} />
+          {session?.isLoggedIn && (
+              <NavProjects projects={data.projects} />
+          )}
+          <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
           {session?.isLoggedIn && (
