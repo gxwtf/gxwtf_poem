710ac78414690fb3362d29ef4887ed79fcf162d1
Wangzehao2009 <15866374+wangzehao2009@user.noreply.gitee.com>
2025-08-25 22:58:59 +0800

    add: add tags

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -2,21 +2,45 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 
 interface TagProps {
-  text: string
-  href?: string
+    text: string
+    href?: string
 }
 
 export function Tag({ text, href }: TagProps) {
-  if (href) {
+    if (href) {
+        return (
+            <Button asChild size="sm" variant="outline">
+                <Link
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >{text}
+                </Link>
+            </Button>
+        )
+    }
     return (
-      <Button asChild size="sm" variant="outline">
-        <Link href={href}>{text}</Link>
-      </Button>
+        <Button size="sm" variant="outline">
+            {text}
+        </Button>
     )
-  }
-  return (
-    <Button size="sm" variant="outline">
-      {text}
-    </Button>
-  )
+}
+
+interface TagsProps {
+    tags: string[]
+    className?: string
+}
+
+export function Tags({ tags, className = "" }: TagsProps) {
+    return (
+        <div className={`text-primary flex flex-wrap gap-2 ${className}`}>
+            {tags.map((tag, index) => (
+                <Tag
+                    key={index}
+                    text={tag}
+                    href={`/tag/${tag}`}
+                />
+            ))}
+        </div>
+    )
 }
