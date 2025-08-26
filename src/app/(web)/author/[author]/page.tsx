4bb7f44a85f8debe2699ac6106c43a95c65dc579
Wangzehao2009 <15866374+wangzehao2009@user.noreply.gitee.com>
2025-08-26 20:36:05 +0800

    update: update author & comb preview page

---
+++ src/app/(web)/author/[author]/page.tsx
@@ -0,0 +1,35 @@
+// 作者介绍页面
+import { notFound } from 'next/navigation'
+import { MDXPreview } from '@/components/mdx-preview'
+import { loadMDXWithTOC } from '@/lib/mdx-utils'
+
+interface Props {
+    params: Promise<{ author: string }>;
+}
+
+export async function generateMetadata(props: Props) {
+    const { author } = await props.params;
+    const Author = decodeURIComponent(author);
+    return {
+        title: `作者介绍 - ${Author}`,
+        description: '作者介绍页面',
+    };
+}
+
+export default async function Page(props: Props) {
+    const { author } = await props.params;
+    const Author = decodeURIComponent(author);
+    try {
+        const { mdxComponent: IntroMDX, toc } = await loadMDXWithTOC(`author/${Author}/intro.mdx`);
+        return (
+            <MDXPreview
+                mdxContent={<IntroMDX />}
+                toc={toc}
+                headerData={[{ name: "作者", href: "/authors" }]}
+                now={Author}
+            />
+        );
+    } catch (error) {
+        notFound();
+    }
+}
