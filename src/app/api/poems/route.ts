b69798d9a2529dac646b8557cfab282067e5e10e
kelly <kellynew@outlook.com>
2025-09-07 22:13:38 +0800

    fix: prisma client NOTE: YOU NEED TO RERUN NPX PRISMA GENERATE AFTER APPLYING THIS COMMIT

--- src/app/api/poems/route.ts
+++ src/app/api/poems/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server"
-import { PrismaClient } from "@/app/generated/prisma"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
