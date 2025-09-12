4e19f49fca8f1d18e13a6c486b3c3d0f1047afc7
Wangzehao2009 <15866374+wangzehao2009@user.noreply.gitee.com>
2025-09-12 22:29:47 +0800

    fix: 更新文章图片链接以确保正确显示

--- src/app/(web)/dashboard/page.tsx
+++ src/app/(web)/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
         <>
             <SiteHeader />
             <div className="flex flex-1 flex-col gap-4 p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3 p-6">
                     <CheckIn />
                     <div className="bg-muted/50 aspect-video rounded-xl" />
                     <TodayInHistory />
