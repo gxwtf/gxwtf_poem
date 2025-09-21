c6df041b24e7e628c280041ae9e33c26d622e31c
wchengk09 <wchengk09@163.com>
2025-09-21 21:06:24 +0800

    renamed index.json -> preview.json

--- scripts/poem-content-wck/deepseek.mjs
+++ scripts/poem-content-wck/deepseek.mjs
@@ -1,20 +1,23 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-	baseURL: "https://openrouter.ai/api/v1",
-	apiKey: "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"
+	// baseURL: "https://openrouter.ai/api/v1",
+	// apiKey: "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"
+	baseURL: "http://gxwtf.cn:8000/v1",
+	apiKey: "1145141919810"
 });
 
 function removeThinkTags(input) {
     // 创建正则表达式匹配 <think> 和 </think> 之间的所有内容
-    const regex = /<think>[\s\S]*?<\/think>/g;
+    const regex1 = /<think>[\s\S]*?<\/think>/g;
+	const regex2 = /<reason>[\s\S]*?<\/reason>/g;
     
     // 使用空字符串替换所有匹配项
-    return input.replace(regex, '');
+    return input.replace(regex1, '').replace(regex2, '');
 }
 
 
-export default async function deepseekChat(prompt, model = "deepseek/deepseek-r1-0528:free") {
+export default async function deepseekChat(prompt, unused) {
 	console.log('DeepSeek Chat: ' + prompt);
 
 	try {
@@ -22,7 +25,7 @@ export default async function deepseekChat(prompt, model = "deepseek/deepseek-r1
 			// model: "qwen/qwen3-235b-a22b:free",
 			// model: "deepseek/deepseek-r1-0528:free",
 			// model: "deepseek/deepseek-chat-v3.1:free",
-			model: model,
+			model: 'Qwen3-235B-A22B-Instruct-2507-FP8-DYNAMIC',
 			messages: [{ role: "user", content: prompt }],
 			stream: true
 		});
