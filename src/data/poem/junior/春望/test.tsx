8b92860bab1c6f9955baec319e808856b797ed83
wchengk09 <wchengk09@163.com>
2025-09-25 11:26:20 +0800

    fixed some bugs

---
+++ src/data/poem/junior/春望/test.tsx
@@ -0,0 +1,22 @@
+import { Meta } from '@/components/poem-meta';
+import { PoemPreview } from '@/components/poem-preview/poem-preview';
+import fullData from './full.json';
+import {MemorizeContextProvider} from "@/components/poem-preview/memorize-context"
+import full2preview, {InputData} from "@/components/poem-preview/full2json";
+
+function XC(){
+    return <>
+
+
+<Meta 
+  title="XC"
+  author="XC"
+  dynasty="XC"
+/>
+
+<MemorizeContextProvider>
+  <PoemPreview data={{mode: (fullData as InputData).mode,preview: full2preview(fullData as InputData).preview}}
+  />
+</MemorizeContextProvider>
+    </>
+}
