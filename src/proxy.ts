4924ba592249b4f9a53eb355210a2b51d240820f
kelly <kellynew@outlook.com>
2025-11-30 10:58:21 +0800

    update(breaking): update NEXT.JS to 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,7 +5,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const username = "gxwtf"
   const password = "gxwtf_2714"
 
